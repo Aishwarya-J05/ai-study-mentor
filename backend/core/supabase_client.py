fe8b630b5247e31668b8c29053ee8afcb7c62181
Aishwarya Joshi <aishwaryajoshi554@gmail.com>
2025-11-23 11:31:16 +0530

    Delete backend/core/supabase_client.py

--- backend/core/supabase_client.py
+++
@@ -1,6 +0,0 @@
-from supabase import create_client, Client
-
-SUPABASE_URL = "https://mfzmvppfjdmhrinxbats.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mem12cHBmamRtaHJpbnhiYXRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MDI1NTMsImV4cCI6MjA3OTM3ODU1M30.53lMVT8bghETnWAW4ZPe9rQJTF3JMoOQtddR7hFa74Y"   # put your actual anon key
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
